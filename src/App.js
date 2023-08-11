63901fde5411865b7e0e6680e04939cc5f5906b1
Gonzalo Suarez <gonzasuarezgagliardi@gmail.com>
2023-08-11 18:49:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { getFirestore } from 'firebase/firestore';
+import Navbar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import { CartProvider } from './components/CartContext';
+import Cart from './components/Cart';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.css';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXmxb0pJg05tFBng2bMeRGk-OyLdz5xWI",
+  authDomain: "compugarza-db0b9.firebaseapp.com",
+  projectId: "compugarza-db0b9",
+  storageBucket: "compugarza-db0b9.appspot.com",
+  messagingSenderId: "935161468294",
+  appId: "1:935161468294:web:f399b14772f16c19f28807"
+};
+
+initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <CartProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="footer-col-2">
+                        <p>Lo más barato de la red al alcance de tus manos!</p>
+                    </div>
+                </div>
+                <hr />
+                <p className="copyright">Creado por Gonzalo Suárez</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
